6ea4148cfbdc973eb039e887694f74b7531c70d9
SaurashisBose24 <saurashisbose@gmail.com>
2025-03-29 01:13:03 +0530

    modified vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
   server: {
     host: true, // Allows access from external networks
     strictPort: true, // Ensures the same port is used
+    https: false, 
     allowedHosts: ['.ngrok-free.app'] // Allow all ngrok subdomains
+  },
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
   }
 })
